b768afdefb4b59fd4f6a6f64895552821b1e2281
Justin Lam <justin.kh.lam@gmail.com>
2021-08-18 20:29:03 +0000

    after refactor and creation of client.js, add handleUserInput

--- play.js
+++ play.js
@@ -3,3 +3,20 @@ const { connect } = require('./client');
 // connecting to server
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() { // set up input
+  const stdin = process.stdin; // shorten
+  stdin.setRawMode(true); // raw value from console
+  stdin.setEncoding('utf8'); // text encoding
+  stdin.resume(); 
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
+
+setupInput();
