d44b702056066068d54d5b3e949b37cf8d222c02
Justin Lam <justin.kh.lam@gmail.com>
2021-08-19 04:18:04 +0000

    add connection variable

--- input.js
+++ input.js
@@ -2,9 +2,11 @@ const { connect } = require('./client');
 
 let connVar = connect(); 
 // Step 2 - Call connect() and then load function into variable
-// Step3 in input.js
 
-const setupInput = function() { // set up input
+let connection;
+
+const setupInput = function(conn) { // set up input
+  connection = conn;
   const stdin = process.stdin; // shorten
   stdin.setRawMode(true); // raw value from console
   stdin.setEncoding('utf8'); // text encoding
@@ -15,20 +17,24 @@ const setupInput = function() { // set up input
 
 // Step 3 - Access connect and pass input below
 const handleUserInput = function(key) {
-  console.log(key);
+  // console.log(key); // track input
   if (key === '\u0003') {
     process.exit();
   };
   if (key === 'w') {
+    console.log(`Up`)
     connVar.write(`Move: up`);
   };
   if (key === 'a') {
+    console.log(`Left`)
     connVar.write(`Move: left`);
   };
   if (key === 's') {
+    console.log(`Down`)
     connVar.write(`Move: down`);
   };
   if (key === 'd') {
+    console.log(`Right`)
     connVar.write(`Move: right`);
   };
 };
